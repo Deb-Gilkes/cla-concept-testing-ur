5603cab55f0d9715d73583e6c531deda835a036e
kateweir1 <110602089+kateweir1@users.noreply.github.com>
2024-03-14 11:44:51 +0000

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
+
+window.GOVUKPrototypeKit.documentReady(() => {
+  // Add JavaScript here
+
+  <div>
+  <script src="/javascripts/vendor/iframeResizer.contentWindow.js"></script>
+  <script src="/javascripts/govuk-frontend-e806d3a2c8879d6de8c1da9933509428.js"></script>
+  <script src="/javascripts/example-9c3169e4fadb985206ba331be236abd9.js"></script>
+  </div>
+})
